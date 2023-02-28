8e8a65dacb15cee4a3a42eff215bc0e6fb22295e
shiva sharma <shva.sharma29@gmail.com>
2023-02-28 20:22:23 +0530

    added delete and getOrder routes

--- router/routes.js
+++ router/routes.js
@@ -4,6 +4,7 @@ const express = require("express");
 const router = express.Router();
 const orderModel = require('../models/orderModel');
 
+//for create the order
 router.post('/postorder',async(req,res)=>{
     const {
         userId , totalQuantity ,totalPrice,   storeLocation ,city,address,phoneNo,shirts,tShirts,trousers, jeans,  boxers,joggers
@@ -19,5 +20,35 @@ router.post('/postorder',async(req,res)=>{
     }else{
         res.status(400).send("Feield to create order!")
     }
+})
+
+//to get the existing orders
+router.get('/getOrder', async (req,res)=>{
+    try{
+        let userExist = await UserModel.findById(req.user.userId)
+        if(!userExist){
+            res.status(400).send("User not exist");
+            res.end();
+        }
+        const result = await orderModel.findById({userId:req.user.userId});
+        res.status(200).json(result);
+
+    }catch(err){
+        res.status(400).send("server error");
+    }
+})
+
+router.delete("deleteOrder/:orderId", (req,res)=>{
+    orderModel.findByIdAndDelete(req.params.orderId, (err,docs)=>{
+        if(err){
+            res.send(err);
+        }
+        else{
+            res.json({
+                success:true,
+                message:"Order deleted"
+            })
+        }
+    })
 })
 module.exports = router;
