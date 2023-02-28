75fa60c8adf3642cbc176b2f798d565d8d53f4fc
Prasanta Mandal <pm863390@gmail.com>
2023-02-28 19:20:40 +0530

    get api for order list

--- router/routes.js
+++ router/routes.js
@@ -1,9 +1,13 @@
 const express = require("express");
 
 
+
 const router = express.Router();
 const orderModel = require('../models/orderModel');
+const UserModel = require('../models/registerModel');
+const ProductModel = require('../models/productTypeModel');
 
+// Post API for postorder
 router.post('/postorder',async(req,res)=>{
     const {
         userId , totalQuantity ,totalPrice,   storeLocation ,city,address,phoneNo,shirts,tShirts,trousers, jeans,  boxers,joggers
@@ -20,4 +24,14 @@ router.post('/postorder',async(req,res)=>{
         res.status(400).send("Feield to create order!")
     }
 })
+
+//get api for order list
+
+router.get("/", (req,res)=>{
+	ProductModel.find({}, (err, docs)=>{
+	
+	res.json(docs);
+})
+})
+
 module.exports = router;
